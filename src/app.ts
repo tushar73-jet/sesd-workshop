8fd55c89cd50d85cfa2582a61d8d606fcfae1e7a
Tushar Kumar <184378329+tushar73-jet@users.noreply.github.com>
2026-02-02 13:40:54 +0530

    Wire Product CRUD routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import express from "express";
+import { ProductRepository } from "./repository";
+import { ProductService } from "./service";
+import { ProductController } from "./controller";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("SESD Backend Running");
-});
+
+const repo = new ProductRepository();
+const service = new ProductService(repo);
+const controller = new ProductController(service);
+
+
+app.get("/products", controller.getAll);
+app.get("/products/:id", controller.getOne);
+app.post("/products", controller.create);
+app.put("/products/:id", controller.update);
+app.delete("/products/:id", controller.delete);
 
 const PORT = 3000;
 app.listen(PORT, () => {
